5f3fbbca878fa72734d28eee0b2a9df356e8513e
hwiyoung <huddy1204@gmail.com>
2019-09-05 11:24:09 +0900

    test: Extract roll/pitch/yaw from images using pyexiv

--- tests/Exif_test.py
+++ tests/Exif_test.py
@@ -2,6 +2,8 @@ import gdal
 from PIL import Image
 # import exifread
 import pyexiv2
+import os
+import math
 
 if __name__ == '__main__':
     file_path1 = './testData/20180213_064947.tiff'
@@ -27,34 +29,59 @@ if __name__ == '__main__':
     #    if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
     #        print("Key: %s, value %s" % (tag, tags[tag]))
 
-    ### pyexiv2
-    metadata = pyexiv2.ImageMetadata(file_path5)
-    metadata.read()
-    print(metadata.exif_keys)
-    # print(metadata.xmp_keys)
+    # ### pyexiv2
+    # metadata = pyexiv2.ImageMetadata(file_path5)
+    # metadata.read()
+    # print(metadata.exif_keys)
+    # # print(metadata.xmp_keys)
+    #
+    # latitude = metadata['Exif.GPSInfo.GPSLatitude']
+    # latitudeValue = latitude.raw_value.split('/')
+    # latitudeDeg = int(latitudeValue[0])
+    # latitudeMin = int(latitudeValue[1].split(' ')[1])
+    # latitudeSec = int(latitudeValue[2].split(' ')[1]) / 1000
+    # lat = latitudeDeg + latitudeMin/60 + latitudeSec/3600
+    #
+    # longitude = metadata['Exif.GPSInfo.GPSLongitude']
+    # longitudeValue = longitude.raw_value.split('/')
+    # longitudeDeg = int(longitudeValue[0])
+    # longitudeMin = int(longitudeValue[1].split(' ')[1])
+    # longitudeSec = int(longitudeValue[2].split(' ')[1]) / 1000
+    # lon = longitudeDeg + longitudeMin/60 + longitudeSec/3600
+    #
+    # altitude = metadata['Exif.GPSInfo.GPSAltitude']
+    # altitudeValue = altitude.raw_value.split('/')
+    # alt = int(altitudeValue[0])/int(altitudeValue[1])
+    #
+    # focalLength = metadata['Exif.Photo.FocalLength']
+    # sensorWidth = metadata['Exif.Photo.FocalPlaneXResolution']  # row
+    # sensorWidthValue = sensorWidth.raw_value
+    #
+    # print('GPS info: ', lat, " ", lon, " ", alt)
+    # print('Focal Length: ', focalLength.value, 'mm')
+    # print('Sensor Width(Row): ', int(sensorWidthValue[0:5])-10000)
 
-    latitude = metadata['Exif.GPSInfo.GPSLatitude']
-    latitudeValue = latitude.raw_value.split('/')
-    latitudeDeg = int(latitudeValue[0])
-    latitudeMin = int(latitudeValue[1].split(' ')[1])
-    latitudeSec = int(latitudeValue[2].split(' ')[1]) / 1000
-    lat = latitudeDeg + latitudeMin/60 + latitudeSec/3600
+    f = open('RPY003.txt', 'w')
 
-    longitude = metadata['Exif.GPSInfo.GPSLongitude']
-    longitudeValue = longitude.raw_value.split('/')
-    longitudeDeg = int(longitudeValue[0])
-    longitudeMin = int(longitudeValue[1].split(' ')[1])
-    longitudeSec = int(longitudeValue[2].split(' ')[1]) / 1000
-    lon = longitudeDeg + longitudeMin/60 + longitudeSec/3600
+    for root, dirs, files in os.walk('/home/innopam-ldm/hdd/dbrain/20190829_여수_1소티/003'):
+        for file in files:
+            filename = os.path.splitext(file)[0]
+            extension = os.path.splitext(file)[1]
+            file_path = root + '/' + file
 
-    altitude = metadata['Exif.GPSInfo.GPSAltitude']
-    altitudeValue = altitude.raw_value.split('/')
-    alt = int(altitudeValue[0])/int(altitudeValue[1])
+            metadata = pyexiv2.ImageMetadata(file_path)
+            metadata.read()
 
-    focalLength = metadata['Exif.Photo.FocalLength']
-    sensorWidth = metadata['Exif.Photo.FocalPlaneXResolution']  # row
-    sensorWidthValue = sensorWidth.raw_value
+            roll = metadata['Xmp.DLS.Roll']
+            pitch = metadata['Xmp.DLS.Pitch']
+            yaw = metadata['Xmp.DLS.Yaw']
 
-    print('GPS info: ', lat, " ", lon, " ", alt)
-    print('Focal Length: ', focalLength.value, 'mm')
-    print('Sensor Width(Row): ', int(sensorWidthValue[0:5])-10000)
+            rollValue = float(roll.value) * 180 / math.pi
+            pitchValue = float(pitch.value) * 180 / math.pi
+            yawValue = float(yaw.value) * 180 / math.pi
+
+            # print(file, '\t', rollValue, '\t', pitchValue, '\t', yawValue)
+            data = file + '\t' + str(rollValue) + '\t' + str(pitchValue) + '\t' + str(yawValue) + '\n'
+            f.write(data)
+
+    f.close()
