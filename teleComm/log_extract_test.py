60df36641618bea5ec5382e06c791728ca775640
Hwiyoung <huddy1204@gmail.com>
2019-10-21 16:43:09 +0900

    feat: Add codes for extracting logs v2

--- teleComm/log_extract_test.py
+++ teleComm/log_extract_test.py
@@ -1,53 +1,36 @@
 import cv2
 import numpy as np
-# import pandas as pd
-import datetime
+import pandas as pd
 
-#이미지 읽기 기본 메타정보 출력######################################
 cap = cv2.VideoCapture('../../DJI_0018.MOV')
 fps = cap.get(cv2.CAP_PROP_FPS)
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print(frame_count)
-
-#wait_time = int(1000/fps)
 
 print(cap.get(3), cap.get(4))
 print(fps)
 
 count = 0
 
-print('#초기 드론 로그파일(CSV) 읽기###########################################')
-csv_data = np.genfromtxt('DJI_0018_modify_test.csv', delimiter=',', encoding='ascii',
-                         names=('CUSTOM.updateTime', 'CUSTOM.isPhoto', 'CUSTOM.isVideo', 'CUSTOM.hSpeed [m/s]'),
-                        dtype=None)#, comments='#')
+csv_file = 'DJI_0018_modify.csv'
 
-print(csv_data)
+print('********** Read csv file of drone log **********')
+df = pd.read_csv(csv_file, low_memory=False)
 
-#
-# print('#비디오가 녹화되고 있는 시간의 record만 추출##############################')
-# df = df[df['CAMERA_INFO.recordState'] == 'Starting']
-# print(df.head(10))
-#
-# print('#필요한 colum만 추출##################################################')
-# df = df[['CUSTOM.updateTime', 'OSD.latitude', 'OSD.longitude', 'OSD.height [m]', 'OSD.altitude [m]',
-#              'OSD.xSpeed [m/s]', 'OSD.ySpeed [m/s]', 'OSD.zSpeed [m/s]', 'OSD.roll', 'OSD.pitch', 'OSD.yaw',
-#              'CAMERA_INFO.recordState', 'CAMERA_INFO.videoRecordTime',
-#              'GIMBAL.pitch', 'GIMBAL.roll', 'GIMBAL.yaw']]
-#
-# print(df.head(10))
-#
-# print('#index를 0부터 다시 부여함/첫번째 데이터에 대한 처리를 다르게 하기 위해#########')
-# df = df.reset_index(drop=True)
-# print(df.head(10))
-#
-# print('#필요한 변수 생성########################################################')
-# starting_time_str = df.at[0, 'CUSTOM.updateTime']
-# starting_time_obj = datetime.datetime.strptime(starting_time_str, '%Y/%m/%d %H:%M:%S.%f')
-# distance_accumulate = 0
-# diff_yaw_accumulate = 0
-# diff_gim_yaw_accumulate = 0
-#
-# print('#EO 파일 저장을 위한 pandas dataframe 생성################################')
-# image_info = {"imageID":[],"latitude":[], "longitude":[], "altitude":[], "roll":[], "pitch":[], "yaw":[]}
-# print(image_info)
-# df_image_info = pd.DataFrame(image_info)
+print('********** Extract rows on recording **********')
+df = df[df['CAMERA_INFO.recordState'] == 'Starting']
+
+print('********** Extract columns on need **********')
+df = df[['CUSTOM.updateTime', 'OSD.latitude', 'OSD.longitude', 'OSD.height [m]',
+         'OSD.xSpeed [m/s]', 'OSD.ySpeed [m/s]', 'OSD.zSpeed [m/s]',
+         'OSD.roll', 'OSD.pitch', 'OSD.yaw',
+         'GIMBAL.pitch', 'GIMBAL.roll', 'GIMBAL.yaw']]
+
+df_np = df.to_numpy()
+for i in range(df_np[:, 0].shape[0]):
+    df_np[i, 0] = df_np[:, 0][i][11:23]
+    # df_np[i, 0][0:2] = str(int(df_np[i, 0][0:2]) + 9)
+
+logs = np.empty((df_np.shape[0]*3, df_np.shape[1]), dtype=object)
+for i in range(df_np.shape[0]):
+    logs[3*i:3*i+3][:] = df_np[i][:].reshape(1, df_np.shape[1])
