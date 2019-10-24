bcb58dd22a750c83112fe7053935baa199c7f821
hwiyoung <huddy1204@gmail.com>
2019-10-25 01:16:52 +0900

    test: Check extracting exif data

--- tests/Exif_test.py
+++ tests/Exif_test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     file_path2 = '../Data/DJI_0386.JPG'
     file_path3 = './20181018_160439_346.TIFF'
     file_path4 = './testData/20190619_131032_R.tif'
-    file_path5 = './testData/sample dir/20190822/flir/20190822_113709_574.TIFF'
+    file_path5 = './20191011_074853.JPG'
 
     # ## GDAL
     # hDataset = gdal.Open(file_path1, gdal.GA_ReadOnly)
@@ -29,11 +29,11 @@ if __name__ == '__main__':
     #    if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
     #        print("Key: %s, value %s" % (tag, tags[tag]))
 
-    # ### pyexiv2
-    # metadata = pyexiv2.ImageMetadata(file_path5)
-    # metadata.read()
-    # print(metadata.exif_keys)
-    # # print(metadata.xmp_keys)
+    ### pyexiv2
+    metadata = pyexiv2.ImageMetadata(file_path5)
+    metadata.read()
+    print(metadata.exif_keys)
+    print(metadata.xmp_keys)
     #
     # latitude = metadata['Exif.GPSInfo.GPSLatitude']
     # latitudeValue = latitude.raw_value.split('/')
@@ -61,48 +61,48 @@ if __name__ == '__main__':
     # print('Focal Length: ', focalLength.value, 'mm')
     # print('Sensor Width(Row): ', int(sensorWidthValue[0:5])-10000)
 
-    f = open('RPY000.txt', 'w')
-
-    for root, dirs, files in os.walk('/home/innopam-ldm/hdd/dbrain/20190829_여수_1소티/000'):
-        for file in files:
-            filename = os.path.splitext(file)[0]
-            extension = os.path.splitext(file)[1]
-            file_path = root + '/' + file
-
-            if filename.split('_')[2] == '1':
-
-                metadata = pyexiv2.ImageMetadata(file_path)
-                metadata.read()
-
-                latitude = metadata['Exif.GPSInfo.GPSLatitude']
-                latitudeValue = latitude.raw_value.split('/')
-                latitudeDeg = int(latitudeValue[0]) / int(latitudeValue[1].split(' ')[0])
-                latitudeMin = int(latitudeValue[1].split(' ')[1]) / int(latitudeValue[2].split(' ')[0])
-                latitudeSec = int(latitudeValue[2].split(' ')[1]) / int(latitudeValue[3].split(' ')[0])
-                lat = latitudeDeg + latitudeMin/60 + latitudeSec/3600
-
-                longitude = metadata['Exif.GPSInfo.GPSLongitude']
-                longitudeValue = longitude.raw_value.split('/')
-                longitudeDeg = int(longitudeValue[0]) / int(longitudeValue[1].split(' ')[0])
-                longitudeMin = int(longitudeValue[1].split(' ')[1]) / int(longitudeValue[2].split(' ')[0])
-                longitudeSec = int(longitudeValue[2].split(' ')[1]) / int(longitudeValue[3].split(' ')[0])
-                lon = longitudeDeg + longitudeMin/60 + longitudeSec/3600
-
-                altitude = metadata['Exif.GPSInfo.GPSAltitude']
-                altitudeValue = altitude.raw_value.split('/')
-                alt = int(altitudeValue[0])/int(altitudeValue[1])
-
-                roll = metadata['Xmp.DLS.Roll']
-                pitch = metadata['Xmp.DLS.Pitch']
-                yaw = metadata['Xmp.DLS.Yaw']
-
-                rollValue = float(roll.value) * 180 / math.pi
-                pitchValue = float(pitch.value) * 180 / math.pi
-                yawValue = float(yaw.value) * 180 / math.pi
-
-                # print(file, '\t', rollValue, '\t', pitchValue, '\t', yawValue)
-                data = filename + '\t' + str(lon) + '\t' + str(lat) + '\t' + str(alt) + '\t' + \
-                       str(rollValue) + '\t' + str(pitchValue) + '\t' + str(yawValue) + '\n'
-                f.write(data)
-
-    f.close()
+    # f = open('RPY000.txt', 'w')
+    #
+    # for root, dirs, files in os.walk('/home/innopam-ldm/hdd/dbrain/20190829_여수_1소티/000'):
+    #     for file in files:
+    #         filename = os.path.splitext(file)[0]
+    #         extension = os.path.splitext(file)[1]
+    #         file_path = root + '/' + file
+    #
+    #         if filename.split('_')[2] == '1':
+    #
+    #             metadata = pyexiv2.ImageMetadata(file_path)
+    #             metadata.read()
+    #
+    #             latitude = metadata['Exif.GPSInfo.GPSLatitude']
+    #             latitudeValue = latitude.raw_value.split('/')
+    #             latitudeDeg = int(latitudeValue[0]) / int(latitudeValue[1].split(' ')[0])
+    #             latitudeMin = int(latitudeValue[1].split(' ')[1]) / int(latitudeValue[2].split(' ')[0])
+    #             latitudeSec = int(latitudeValue[2].split(' ')[1]) / int(latitudeValue[3].split(' ')[0])
+    #             lat = latitudeDeg + latitudeMin/60 + latitudeSec/3600
+    #
+    #             longitude = metadata['Exif.GPSInfo.GPSLongitude']
+    #             longitudeValue = longitude.raw_value.split('/')
+    #             longitudeDeg = int(longitudeValue[0]) / int(longitudeValue[1].split(' ')[0])
+    #             longitudeMin = int(longitudeValue[1].split(' ')[1]) / int(longitudeValue[2].split(' ')[0])
+    #             longitudeSec = int(longitudeValue[2].split(' ')[1]) / int(longitudeValue[3].split(' ')[0])
+    #             lon = longitudeDeg + longitudeMin/60 + longitudeSec/3600
+    #
+    #             altitude = metadata['Exif.GPSInfo.GPSAltitude']
+    #             altitudeValue = altitude.raw_value.split('/')
+    #             alt = int(altitudeValue[0])/int(altitudeValue[1])
+    #
+    #             roll = metadata['Xmp.DLS.Roll']
+    #             pitch = metadata['Xmp.DLS.Pitch']
+    #             yaw = metadata['Xmp.DLS.Yaw']
+    #
+    #             rollValue = float(roll.value) * 180 / math.pi
+    #             pitchValue = float(pitch.value) * 180 / math.pi
+    #             yawValue = float(yaw.value) * 180 / math.pi
+    #
+    #             # print(file, '\t', rollValue, '\t', pitchValue, '\t', yawValue)
+    #             data = filename + '\t' + str(lon) + '\t' + str(lat) + '\t' + str(alt) + '\t' + \
+    #                    str(rollValue) + '\t' + str(pitchValue) + '\t' + str(yawValue) + '\n'
+    #             f.write(data)
+    #
+    # f.close()
