19ddc0361a5d12aea3814f06f65604eddf4cdb26
Hwiyoung <huddy1204@gmail.com>
2019-11-11 14:21:06 +0900

    test: Add a module for mosaic(2)

--- tests/Mosaic_test.py
+++ tests/Mosaic_test.py
@@ -1,13 +1,17 @@
 import os
 import subprocess
 
-working_path = '../OTB-7.0.0-Linux64/'
-set_env = 'source'# ./otbenv.profile'
-mosaic_execution = 'otbcli_Mosaic'
+working_path1 = '../OTB-7.0.0-Linux64/'
+working_path2 = './bin/'
+set_env = './otbenv.profile'
+mosaic_execution = './otbcli_Mosaic'
 
 #change path
-os.chdir(working_path)
+os.chdir(working_path1)
 subprocess.call('ls')
-
+# https://stackoverflow.com/questions/13702425/source-command-not-found-in-sh-shell/13702876
 subprocess.call(set_env, shell=True)
+
+os.chdir(working_path2)
+subprocess.call('ls')
 subprocess.call(mosaic_execution)
