b86ed7bdf11276f3545cb8237679ba07df1a1e26
hwiyoung <huddy1204@gmail.com>
2019-11-21 22:32:35 +0900

    test: Check if a function is called or not

---
+++ test/concur_send_test.py
@@ -0,0 +1,9 @@
+import socket
+from struct import *
+
+s0 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s0.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+dest = ("localhost", 57810)
+
+s0.sendto(b'START', dest)
+print('Sent!')
---
+++ test/concurrent_test.py
@@ -0,0 +1,41 @@
+# https://dzone.com/articles/python-how-to-tell-if-a-function-has-been-called
+import functools
+import socket
+
+def calltracker(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        wrapper.has_been_called = True
+        return func(*args)
+    wrapper.has_been_called = False
+    return wrapper
+
+@calltracker
+def doubler(number):
+    check = 0
+    while check < 50000000:
+        check += 1
+    print(check)
+
+
+if __name__ == '__main__':
+    while True:
+        ################################
+        # Server for path, frame, bbox #
+        ################################
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('localhost', 57810))
+        print("binding...")
+
+        data, addr = s.recvfrom(200)
+
+        header = data[:5]
+        if header == b'START':
+            # doubler(2)  # if a function is called... print('doubler has been called!')
+            # if a function is NOT called... print("You haven't called this function yet")
+            if not doubler.has_been_called:
+                print("You haven't called this function yet")
+                doubler(2)
+            if doubler.has_been_called:
+                print('doubler has been called!')
