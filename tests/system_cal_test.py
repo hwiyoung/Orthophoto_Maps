6fe021abd9fa5707155280d09d0c6250c3e5551b
Hwiyoung <huddy1204@gmail.com>
2020-02-24 18:13:55 +0900

    test: Check the formula between opk and smartphone rpy

--- tests/system_cal_test.py
+++ tests/system_cal_test.py
@@ -1,8 +1,24 @@
 from pyexiv2 import metadata
 import os
 import numpy as np
+from copy import copy
 
-f = open('../../Smartphone_Image_opk/rpy.txt', "w")
+def rot_2d(theta):
+    # Convert the coordinate system not coordinates
+    return np.array([[np.cos(theta), np.sin(theta)],
+                     [-np.sin(theta), np.cos(theta)]])
+
+def rpy_to_opk_test(smartphone_rpy):
+    roll_pitch = copy(smartphone_rpy[0:2])
+
+    roll_pitch[0] = -smartphone_rpy[1]
+    roll_pitch[1] = -smartphone_rpy[0]
+
+    omega_phi = np.dot(rot_2d(smartphone_rpy[2] * np.pi / 180), roll_pitch.reshape(2, 1))
+    kappa = -smartphone_rpy[2]-90
+    return np.array([float(omega_phi[0, 0]), float(omega_phi[1, 0]), kappa])
+
+# f = open('../../Smartphone_Image_opk/rpy.txt', "w")
 for root, dirs, files in os.walk('../../Smartphone_Image_opk'):
     files.sort()
     for file in files:
@@ -19,8 +35,10 @@ for root, dirs, files in os.walk('../../Smartphone_Image_opk'):
         yaw = float(meta['Xmp.DLS.Yaw'].value) * 180 / np.pi
         print(roll, pitch, yaw)
         data = file + "\t" + str(roll) + "\t" + str(pitch) + "\t" + str(yaw) + "\n"
-        f.write(data)
+        # f.write(data)
+
+        opk = rpy_to_opk_test(np.array([roll, pitch, yaw]))
 
         print()
 
-f.close()
+# f.close()
